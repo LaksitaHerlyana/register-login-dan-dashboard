faeb58b7ada0ab33746ee88963b305217825c301
LaksitaHerlyana <laksita_purnomo_33rpl@student.smktelkom-mlg.sch.id>
2026-05-22 10:29:27 +0700

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,10 @@
+const UserPage = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Kelola User</h1>
+      <p>Halaman untuk mengelola user</p>
+    </div>
+  );
+}
+
+export default UserPage;
